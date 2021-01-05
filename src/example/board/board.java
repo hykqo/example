8c95299bc903f20404d52e38f1e2fa5a64861cdd
ENO <hykqo@naver.com>
2021-01-05 13:11:48 +0900

    Revert "0105 -12:59"
    This reverts commit 83d5b1d0e3e0f2876601a153127c1a85df33cdeb.

--- src/example/board/board.java
+++ src/example/board/board.java
@@ -2,13 +2,4 @@ package example.board;
 
 public class board {
 
-	
-private int board_no;
-private int board_aasdf;
-private int board_123;
-private int board_asd;
-private int board_dsfg;
-private int board_sdf;
-
-
 }
