83d5b1d0e3e0f2876601a153127c1a85df33cdeb
ENO <hykqo@naver.com>
2021-01-05 13:00:07 +0900

    0105 -12:59

--- src/example/board/board.java
+++ src/example/board/board.java
@@ -2,4 +2,13 @@ package example.board;
 
 public class board {
 
+	
+private int board_no;
+private int board_aasdf;
+private int board_123;
+private int board_asd;
+private int board_dsfg;
+private int board_sdf;
+
+
 }
